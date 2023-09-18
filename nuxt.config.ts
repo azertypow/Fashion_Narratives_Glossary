a0ee223a335a558cf71021c25c684500a6ace987
azertypow <azertypow@gmail.com>
2023-09-18 17:57:55 +0200

    feat: github page settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: [
-      '~/assets/_main.scss',
-  ],
+    devtools: {enabled: true},
+    app: {
+        baseURL: "/Fashion_Narratives_Glossary/",
+    },
+    css: [
+        '~/assets/_main.scss',
+    ],
 })
